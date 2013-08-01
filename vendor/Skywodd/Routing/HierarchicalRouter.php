248a300f0e8f82fdeececa703e32cdb71d6bf6e2
Skywodd <skywodd@gmail.com>
2013-08-01 15:17:14 +0200

    Add HMVC router

---
+++ vendor/Skywodd/Routing/HierarchicalRouter.php
@@ -0,0 +1,252 @@
+<?php
+
+/**
+ * Hierarchical router for HMVC system design
+ * 
+ * @author Fabien Batteix <skywodd@gmail.com>
+ * @copyright Fabien Batteix 2013
+ * @link http://skywodd.net
+ * @package skywebsite
+ */
+/*
+ * This file is part of Skywodd website.
+ * 
+ * Skywodd website is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Skywodd website is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Skywodd website. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* PSR-0 compliant namespace */
+namespace Skywodd\Routing;
+
+/**
+ * Hierarchical router part of HMVC system design
+ *
+ * @version 1.0
+ */
+class HierarchicalRouter {
+
+    /**
+     * Controller files extension
+     * 
+     * @var string
+     */
+    const CONTROLLER_EXTENSION = '.php';
+    
+    /**
+     * The directory where all controller files are stored
+     * 
+     * @var string
+     */
+    protected $_workingDirectory = '';
+
+    /**
+     * Default home controller name
+     * 
+     * @var string 
+     */
+    protected $_defaultHomeControllerName = '';
+
+    /**
+     * Default home path node
+     * 
+     * @var string 
+     */
+    protected $_defaultHomePathNode = '';
+
+    /**
+     * Default error controller name
+     * 
+     * @var string 
+     */
+    protected $_defaultErrorControllerName = '';
+
+    /**
+     * List of registered routes
+     * 
+     * @var Route[]
+     */
+    protected $_registeredRoutes = [];
+
+    /**
+     * Instantiate a new HierarchicalRouter
+     *
+     * @param string $workingDirectory The directory where all controller files are stored
+     */
+    public function __construct($workingDirectory) {
+
+        /* Assert argument */
+        assert('isset($workingDirectory) && is_string($workingDirectory)');
+        assert('is_dir($workingDirectory) && is_readable($workingDirectory)');
+
+        /* Store the controllers directory path */
+        $this->_workingDirectory = $workingDirectory;
+    }
+
+    /**
+     * Set the default home controller name
+     * The home controller will be called when the associated path node is requested or when no path node is specified.
+     * 
+     * @since 1.0
+     * @param string $controllerName The new default home controller name
+     * @param string $homePathNode The path node associated with the home controller
+     */
+    public function setDefaultHomeController($controllerName, $homePathNode = '') {
+
+        /* Assert arguments */
+        assert('isset($controllerName) && is_string($controllerName)');
+        assert('isset($homePathNode) && is_string($homePathNode)');
+        assert('empty($controllerName) === FALSE');
+
+        /* Store the default home controller name and path node */
+        $this->_defaultHomeControllerName = $controllerName;
+        $this->_defaultHomePathNode = trim($homePathNode, '/');
+    }
+
+    /**
+     * Set the default error controller name
+     * The error controller will be called when an unknown path node is requested.
+     * 
+     * @since 1.0
+     * @param type $controllerName The new default error controller name
+     */
+    public function setDefaultErrorController($controllerName) {
+
+        /* Assert argument */
+        assert('isset($controllerName) && is_string($controllerName)');
+        assert('empty($controllerName) === FALSE');
+
+        /* Store the default error controller name */
+        $this->_defaultErrorControllerName = $controllerName;
+    }
+
+    /**
+     * Add a new routing rule to the router
+     * This function should be called as many times as necessary.
+     * 
+     * @since 1.0
+     * @param Route $r
+     */
+    public function addRoute(Route $r) {
+
+        /* Assert argument */
+        assert('isset($r)');
+
+        /* Append the given route to the routes array */
+        $this->_registeredRoutes[] = $r;
+    }
+
+    /**
+     * Run the router on the specified ressource path
+     * Do not forget to use the rebaseRessourcePath() function if you are not working in the web-root directory.
+     * 
+     * @since 1.0
+     * @param string $ressourcePath The input ressource path to process, should be rebased if necessary
+     */
+    public function processRessourcePath($ressourcePath) {
+
+        /* Assert argument */
+        assert('isset($ressourcePath) && is_string($ressourcePath)');
+        assert('empty($this->_defaultHomeControllerName) === FALSE');
+        assert('empty($this->_defaultErrorControllerName) === FALSE');
+
+        /* Trim slash at beginning of ressource path */
+        $ressourcePath = trim($ressourcePath, '/');
+
+        /* Get the first path node and remaining path string */
+        $pathNodes = explode('/', $ressourcePath, 2);
+        $pathNode = $pathNodes[0];
+        $remainingPath = (count($pathNodes) == 2) ? $pathNodes[1] : '';
+
+        /* Check for default home controller */
+        if ($pathNode == '' || $pathNode == $this->_defaultHomePathNode) {
+
+            /* Call the default home controller */
+            $this->callController($this->_defaultHomeControllerName, $remainingPath);
+            return;
+        }
+
+        /* Try to match any of the registered routes */
+        foreach ($this->_registeredRoutes as $r) {
+
+            /* If current route match */
+            if ($r->match($pathNode)) {
+
+                /* Call the associated controller */
+                $this->callController($r->getControllerName(), $remainingPath);
+                return;
+            }
+        }
+
+        /* No match found, call the default error controller */
+        $this->callController($this->_defaultErrorControllerName, '');
+    }
+
+    /**
+     * Internal routine : instantiate and execute the specified controller
+     * 
+     * @since 1.0
+     * @param string $controllerName The controller name (can be in a sub-directory) without the .php extension
+     * @param string $controllerArgs The arguments to pass to the controller during the instantiation
+     */
+    protected function callController($controllerName, $controllerArgs) {
+
+        /* Assert arguments */
+        assert('isset($controllerName) && is_string($controllerName)');
+        assert('isset($controllerArgs) && is_string($controllerArgs)');
+
+        /* Compute controller filename */
+        $controllerFilename = $this->_workingDirectory . DIRECTORY_SEPARATOR . $controllerName . self::CONTROLLER_EXTENSION;
+
+        /* Assert controller file */
+        assert('is_file($controllerFilename) && is_readable($controllerFilename)');
+
+        /* Include the controller file */
+        require $controllerFilename;
+
+        /* Compute controller class name */
+        $controllerClassName = basename($controllerFilename, self::CONTROLLER_EXTENSION);
+
+        /* Instanciate controller */
+        $controller = new $controllerClassName($controllerArgs);
+
+        /* Execute the controller */
+        $controller->execute();
+    }
+
+    /**
+     * Rebase (chroot like) a ressource path
+     * Turn absolute ressource paths like "/foo/bar" into "/bar" according base path (here "/foo").
+     * 
+     * @since 1.0
+     * @param string $ressourcePath The absolute ressource path
+     * @param string $basePath The base path to remove from the absolute ressource path
+     * @return string The rebased ressource path
+     */
+    public static function rebaseRessourcePath($ressourcePath, $basePath) {
+
+        /* Assert arguments */
+        assert('isset($ressourcePath) && is_string($ressourcePath)');
+        assert('isset($basePath) && is_string($basePath)');
+        assert('strlen($basePath) <= strlen($ressourcePath)');
+
+        /* Check the base path */
+        if (substr($ressourcePath, 0, strlen($basePath)) !== $basePath)
+            throw new Exception("Cannot rebase '$ressourcePath' using '$basePath' as base !");
+
+        /* Return the rebased ressource path */
+        return substr($ressourcePath, strlen($basePath));
+    }
+
+}
+
+?>
