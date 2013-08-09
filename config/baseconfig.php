3d13f3fe7553c22a4dc11cb8998ae8d4b670c749
Skywodd <skywodd@gmail.com>
2013-08-09 16:32:06 +0200

    Update readme and add license header

--- config/baseconfig.php
+++ config/baseconfig.php
@@ -1,10 +1,37 @@
 <?php
 
 /**
- * Set to false if the website is not in the root directory
- * (if false don't forget to set the base directory of the website)
+ * Base configuration file, for installation directory settings.
+ * 
+ * This configuration file store all "base directory" settings of the website installation.
+ * 
+ * @author Fabien Batteix <skywodd@gmail.com>
+ * @copyright Fabien Batteix 2013
+ * @link http://skywodd.net My website
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3
  */
-define('BASE_ISROOT', false);
+/*
+ * This file is part of Skywodd website.
+ * 
+ * Skywodd website is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Skywodd website is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Skywodd website. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Set this define to false if the website is not installed in the web-root directory.
+ * (if false don't forget to set the base directory of the website !)
+ */
+define('BASE_IS_ROOT', false);
 
 /**
  * Base directory of the website (without final slash)
